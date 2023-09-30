75bf3802306c3632a2e69d2ba668dbeae9162c0c
arunkumar2511 <arun.pak94@gmail.com>
2023-09-30 14:51:37 +0530

    final changes

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 # After cloining into local please do "npm install"
 # After the successful installation of all dependencies, start the server using command "npm start"
 # Once you see this message "app started listening at 3000" server initiated successfully with the url http://localhost:3000
-
+# http://localhost:3000/api/v1/migrate-rings?N=1&A=A&B=B&C=C this is the get api with sample payload to get rings on given plates and all the payloads are expected as query params . the logic of the function to swap plate is swapping the target and auxillary plate in the parameter of migraterings function which is been called recurssively and pushing the result of each plate to steps function
+# http://localhost:3000/api/v1/fibonacci-series?n=5 this is the get api with sample payload to get fibonacci value of given number n and all the payloads are expected as query params . the logic of the function is a tempm memory variable has been declared which is an object and the same function is been called recurrsively to calculate the sum of fibonacci series for the given input
+# Sample api with payload
+# http://localhost:3000/api/v1/migrate-rings?N=1&A=A&B=B&C=C - GET Method 
+# http://localhost:3000/api/v1/fibonacci-series?n=5 - GET Method
--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const express = require("express");
 const Joi = require("joi");
 const validator = require('express-joi-validation').createValidator({});
-const _ = require("lodash");
 
 const app = express();
 const port = 3000;
 /* Validation Schema for migrate-rings api */
-const migrateSchema = Joi.object({
+const migrateDtoSchema = Joi.object({
     N: Joi.number().positive().greater(0).required(),
     A: Joi.string().required(),
     B: Joi.string().required(),
     C: Joi.string().required(),
 })
 /* Validation Schema for is-balanced api */
-const fibonacciSchema = Joi.object({
-    n: Joi.string().required()
+const fibonacciDtoSchema = Joi.object({
+    n: Joi.number().required()
 })
 app.listen(port,()=>{
     console.log(`app started listening at ${port}`)
 });
 
-app.get('api/v1/migrate-rings',validator.query(migrateSchema),(req,res)=>{
+app.get('api/v1/migrate-rings',validator.query(migrateDtoSchema),(req,res)=>{
     var steps = [];
     const migrateRings = (n,source,target,aux)=>{
         if(n==1){
@@ -33,28 +32,23 @@ app.get('api/v1/migrate-rings',validator.query(migrateSchema),(req,res)=>{
     }
     const {N,A,B,C} = req.query;
     migrateRings(N,A,B,C)
-    return res.status(200).json({
-        status:true,
+    return res.status(200).json({ 
         data:steps
     })
 })
 
-app.get('/api/v1/fibonacci-series',validator.query(fibonacciSchema),(req,res)=>{
+app.get('/api/v1/fibonacci-series',validator.query(fibonacciDtoSchema),(req,res)=>{
     const data = {};
     const fibo = (n)=>{
         if(n <= 1) return 1
-        if(data[n]){
-            return data[n]
-        }
+        if(data[n]) return data[n]
         data[n] = fibo(n-1)+fibo(n-2)
         return data[n]
     }
     var {n} = req.query;
     const result = fibo(n)
-    return res.status(200).json({
-        status:true,
+    return res.status(200).json({ 
         data:`fibonacci value of ${n} is ${result}`
     });
 })
- 
 
